d08b4359c73f898913d44c5656ff9a27171d9ab8
Candy-github <102940442+Candy-github@users.noreply.github.com>
2023-10-28 20:27:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ var config = {
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
         "心爱的小可爱",  // 同上...
         "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
+        "这是你在大学的",
+        "第一个生日",
         "去年的生日",
         "还记得在王婆串串吃的呢",
         "今年要吃好的喽哦",
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        turn_on: "▶点我!!",
+        play: "music🎶",
+        bannar_coming: "别停!",
         balloons_flying: "好像少点东西",
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        wish_message: "დ生日快乐დ",
+        story: "𝓐 𝓜𝓔𝓢𝓢𝓐𝓖𝓔 𝓕𝓞𝓡 𝓨𝓞𝓤 ✎",
     }
 };
